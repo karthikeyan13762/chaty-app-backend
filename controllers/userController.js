dac33a80ed858a8b3f275cdbcd7b814a0b489d58
karthikeyan13762 <karthikeya13762@gmail.com>
2025-01-20 18:24:49 +0530

    Implemented user authentication, messaging functionality, and socket integration-Added user registration and login logic with validation and password hashing-Implemented user avatar setting and profile management-Created message model and controller for adding and retrieving messages-Established routes for user and message operations-Integrated real-time messaging using Socket.io with online user tracking-Configured MongoDB connection and Express server for the API"

---
+++ controllers/userController.js
@@ -0,0 +1,87 @@
+const User = require("../model/useModel");
+const bcrypt = require("bcryptjs");
+
+const register = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    const usernameCheck = await User.findOne({ username });
+    if (usernameCheck) {
+      return res.json({ msg: "Username already used", status: false });
+    }
+    const emailCheck = await User.findOne({ email });
+    if (emailCheck) {
+      return res.json({ msg: "Email already used", status: false });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user)
+      return res.json({ msg: "Incorrect Username or Password", status: false });
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      return res.json({ msg: "Incorrect Username or Password", status: false });
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+const setAvatar = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const avatarImage = req.body.image;
+    const userData = await User.findByIdAndUpdate(
+      userId,
+      {
+        isAvatarImageSet: true,
+        avatarImage,
+      },
+      { new: true }
+    );
+    return res.json({
+      isSet: userData.isAvatarImageSet,
+      image: userData.avatarImage,
+    });
+  } catch (ex) {
+    next(ex);
+  }
+};
+const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({ _id: { $ne: req.params.id } }).select([
+      "email",
+      "username",
+      "avatarImage",
+      "_id",
+    ]);
+    return res.json(users);
+  } catch (ex) {
+    next(ex);
+  }
+};
+const logOut = (req, res, next) => {
+  try {
+    if (!req.params.id) return res.json({ msg: "User id is required " });
+
+    return res.status(200).send({ msg: "User logged out successfully" });
+  } catch (ex) {
+    next(ex);
+  }
+};
+module.exports = { register, login, setAvatar, getAllUsers, logOut };
